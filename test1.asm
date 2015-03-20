4a2a8b28f21744b1696b2df4d83b9e71b74a959c
Agustín Mista <mista.agustin@gmail.com>
2015-03-20 19:07:01 -0300

    Test files now in its own folder

--- test1.asm
+++
@@ -1,9 +0,0 @@
-      mov $20, %r1
-      mov $2, %r2
-      print %r1
-loop:
-      sub %r2, %r1
-      print %r1
-      cmp $0, %r1
-      jmpl loop
-      hlt
--- test2.asm
+++
@@ -1,5 +0,0 @@
-      mov $7, %r1
-      mov $3, %r2
-      and %r2, %r1
-      print %r1
-      hlt
