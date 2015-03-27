d98be4b32b8ba19275cdf3571391bd042b5bafde
paulajessicab <paulajessicab@gmail.com>
2015-03-27 02:03:41 -0300

    Update test2.asm

--- tests/test2.asm
+++ tests/test2.asm
@@ -1,5 +1,14 @@
-  mov $7, %r1
-  mov $3, %r2
-  and %r2, %r1
-  print %r1
+  read %r1
+  mov $0, %r2
+loop:
+  cmp $0, %r1
+  jmpe fin
+  mov $1, %r3
+  and %r1, %r3
+  add %r3, %r2
+  rsh $1, %r1
+  jmp loop
+fin:
+  print %r2
   hlt
+  
